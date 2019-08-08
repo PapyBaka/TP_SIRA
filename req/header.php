94a5f7813e1543c2cbbf05b147bc24cc40a7792e
Florian <zedlike7@gmail.com>
2019-08-08 10:47:39 +0200

    Elaboration du header en cours

---
+++ req/header.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SIRA - LOCATION DE VEHICULES DE PRESTIGES</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="utilities/css/style.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  
+
+  <section>
+  <nav class="navbar navbar-dark bg-dark test">
+    <div class="container">
+      <a class="navbar-brand" href="#">SIRA</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contactez-nous</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="overlay-wcs"></div>
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="utilities/videos/Mercedes Cars Road Trip.mp4" type="video/mp4">
+  </video>
+  <div class="container h-100">
+    <div class="d-flex h-100 text-center align-items-center">
+      <div class="w-100 text-white">
+        <h1>Bienvenue à bord</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">Location de véhicule 24h/24 & 7j/7</h2>
+        <button type="button" class="btn btn-success" href="#">Connexion</button>
+        <button type="button" class="btn btn-success" href="#">Inscription</button>
+      </div>
+    </div>
+  </div>
+</section>
