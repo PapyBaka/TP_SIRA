ecc6a30133559f85995caf26d9cb045e179ddcd0
Rbh91 <53274299+Rbh91@users.noreply.github.com>
2019-08-14 09:48:16 +0200

    Add files via upload

--- req/header.php
+++ req/header.php
@@ -35,23 +35,13 @@
 <body id="page-top">
 
   <!-- Navigation -->
-  
-  
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand navbar-dark bg-dark test">
       <div class="container">
-        <a class="navbar-brand" href="<?= RACINE ?>">SIRA</a>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
-          aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+      <a class="navbar-brand" href="<?= RACINE ?>">SIRA</a>
         <ul class="navbar-nav ml-auto">
-
-        <?php if (is_connected()): ?>
-
-          <?php if ($_SESSION["statut"] == "Admin"): ?>
+    <?php if (is_connected()): ?>
+    
+        <?php if ($_SESSION["statut"] == "Admin"): ?>
           <li class="nav-item">
             <a class="nav-link" href="<?= RACINE . 'admin/gestion_membres.php'?>">Gestion membres</a>
           </li>
@@ -65,16 +55,6 @@
             <a class="nav-link" href="<?= RACINE . 'admin/gestion_agences.php'?>">Gestion agences</a>
           </li>
           <?php else: ?>
-
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-                  <a class="dropdown-item" href="#">My account</a>
-                  <a class="dropdown-item" href="#">Log out</a>
-                </div>
-          </li>
-
           <li class="nav-item">
             <a class="nav-link" href="#">Mon compte</a>
           </li>
@@ -82,25 +62,24 @@
             <a class="nav-link" href="#">Contactez-nous</a>
           </li>
           <?php endif ?>
-         </ul>
+        </ul>
         <ul class="navbar-nav ml-auto nav-flex-icons">
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-user"></i>
+                aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>
             </a>
-            <div class="dropdown-menu dropdown-menu-right dropdown-default"
-              aria-labelledby="navbarDropdownMenuLink-333">
-              <a class="dropdown-item" href="#">Mon compte</a>              
-              <a class="dropdown-item" href="<?= RACINE ."deconnexion.php"?>">Se déconnecter</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <?php else: ?>
-      <ul class="navbar-nav">
-      
-        <li class="nav-item">
+              <div class="dropdown-menu dropdown-menu-right dropdown-default"
+                aria-labelledby="navbarDropdownMenuLink-333">
+                <a class="dropdown-item" href="#">Mon compte</a>              
+                <a class="dropdown-item" href="<?= RACINE ."deconnexion.php"?>">Se déconnecter</a>
+              </div>
+            </li>
+          </ul>      </div>
+    </nav>
+<?php else: ?>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
           <a class="nav-link" href="<?= RACINE."connexion.php"?>">Se connecter</a>
         </li>
         <li class="nav-item">
@@ -115,9 +94,5 @@
       </ul>
     </div>
   </nav>
-      
-    </nav>
-
-
 <?php endif ?>
 <!-- MODAL -->
